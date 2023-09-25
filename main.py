a96169f61143b5cc0d99fe5af9c252b83dc96abb
srinu2003 <tsrin2003@gmail.com>
2023-09-25 15:54:01 +0530

    commit 3

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ from captcha.image import ImageCaptcha
 from io import BytesIO
 
 def main() -> None:
-    text: str = 'HELLO'
+    text: str = 'kill'
 
     captcha: ImageCaptcha = ImageCaptcha(width = 400,
                                          height = 200,
-                                         fonts=['Arial',
-                                                'Perpetua',
-                                                'Rockwell'],
                                          font_sizes=(40, 70, 100))
     captcha.write(text,'captcha.png')
 
